2986b7dc11d0a5d4ae93d23d7721d384eaf78763
Macenissa Mondeguer <mmondeguer@gmail.com>
2024-08-20 12:26:48 +0200

    controller sloop et review

---
+++ app/controllers/sloops_controller.rb
@@ -0,0 +1,25 @@
+class SloopsController < ApplicationController
+  def index
+    @sloops = sloop.all
+  end
+
+  def new
+    @sloop = Sloop.new
+  end
+
+  def create
+    @sloop = Sloop.new(sloop_params)
+    @sloop.user = current_user
+
+    if @sloop.save
+      # redirect_to profil_path(@current_user)
+      redirect_to sloops_path, notice: "Votre sloop a bien été ajoutée"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @sloop = Sloop.find(params[:id])
+  end
+end
---
+++ test/controllers/sloops_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SloopsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
