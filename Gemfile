34794c2666ad7fac3914afa8225921afa3e1f670
david chardon <syhnes@gmail.com>
2024-08-20 11:20:49 +0200

    add seed

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem "rails", "~> 7.1.3", ">= 7.1.3.4"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
+gem 'faker'
+
 # Use postgresql as the database for Active Record
 gem "pg", "~> 1.1"
 
@@ -56,6 +58,8 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mswin mswin64 mingw x64_mingw ]
 end
+
+gem "cloudinary"
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "open-uri"
+
+puts "Cleaning DB..."
+User.destroy_all
+
+puts "Creating users..."
+john = User.new(
+  email: "john@gmail.com",
+  password: "password",
+  description: "Le vent, les embrunts, la mer...",
+  first_name: "John",
+  last_name: "Doe",
+  phone_number: "Faker::PhoneNumber.phone_number_with_country_code"
+  age: "26"
+)
+john.photo.attach(io: file10, filename: "nes.png", content_type: "image/png")
+john.save
