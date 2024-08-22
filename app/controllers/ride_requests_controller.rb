c9dcddaeb1ebea71c95872ed9b32b6b8973a35ae
Macenissa Mondeguer <mmondeguer@gmail.com>
2024-08-22 14:21:17 +0200

    rides_request3

--- app/controllers/ride_requests_controller.rb
+++ app/controllers/ride_requests_controller.rb
@@ -13,15 +13,34 @@ class RideRequestsController < ApplicationController
 
     if @riderequest.save
       # redirect_to profil_path(@current_user)
-      redirect_to riderequests_path, notice: "Votre ridesrequest a bien été ajoutée"
+      redirect_to ride_requests_path, notice: "Votre ridesrequest a bien été ajoutée"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def show
+    @riderequest = RideRequest.find(params[:id])
+
+    @markers = [
+      {
+        lat: @riderequest.start_port_latitude,
+        lng: @riderequest.start_port_longitude,
+        # info_window_html: render_to_string(partial: "info_window", locals: { riderequest: @riderequest }),
+        marker_html: render_to_string(partial: "marker")
+      },
+      {
+        lat: @riderequest.end_port_latitude,
+        lng: @riderequest.end_port_longitude,
+        # info_window_html: render_to_string(partial: "info_window", locals: { riderequest: @riderequest }),
+        marker_html: render_to_string(partial: "marker")
+      }
+    ]
+  end
+
   private
 
   def riderequest_params
-    params.require(:riderequest).permit(:details, :start_date, :end_date, :start_port, :end_port)
+    params.require(:ride_request).permit(:details, :start_date, :end_date, :start_port, :end_port)
   end
 end
--- app/models/ride_request.rb
+++ app/models/ride_request.rb
@@ -3,4 +3,23 @@ class RideRequest < ApplicationRecord
   validates :start_date, :end_date, presence: true
   validates :start_port, :end_port, presence: true
   validates :details, presence: true
+
+  geocoded_by :start_port, latitude: :start_port_latitude, longitude: :start_port_longitude
+
+  after_validation :geocode_start_port, if: :will_save_change_to_start_port?
+  after_validation :geocode_end_port, if: :will_save_change_to_end_port?
+
+  private
+
+  def geocode_start_port
+    geocode
+  end
+
+  # Méthode pour géocoder le port d'arrivée
+  def geocode_end_port
+    return if end_port.blank?
+
+    results = Geocoder.search(end_port)
+    self.end_port_latitude, self.end_port_longitude = results.first.coordinates
+  end
 end
