4f283c61b859d82eee590bd32163ef6b6819bebb
david chardon <syhnes@gmail.com>
2024-08-22 14:58:54 +0200

    b

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @rides = Ride.all.order(:created_at).limit(3)
+    @ridesrequests = RideRequest.all.order(:created_at).limit(3)
   end
 
   def profil
