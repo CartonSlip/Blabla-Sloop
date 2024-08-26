b21cf568ba51537d3b63a0f0cd39520d01af1dda
Thaïs HERVOUËT gh api user/emails | jq -r '.[thais.prinborgne-hervouet@hotmail.fr].email <thais.prinborgne-hervouet@hotmail.fr>
2024-08-26 17:57:42 +0200

    chatroom sans scss

--- app/controllers/user_messages_controller.rb
+++ app/controllers/user_messages_controller.rb
@@ -5,7 +5,13 @@ class UserMessagesController < ApplicationController
     @user_message.chatroom = @chatroom
     @user_message.user = current_user
     if @user_message.save
-      redirect_to chatroom_path(@chatroom)
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.append(:messages, partial: "user_messages/user_message",
+            locals: { user_message: @user_message, user: current_user })
+        end
+        format.html { redirect_to chatroom_path(@chatroom) }
+      end
     else
       render "chatrooms/show", status: :unprocessable_entity
     end
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,4 +2,8 @@ class Chatroom < ApplicationRecord
   belongs_to :user
   belongs_to :ride
   has_many :user_messages
+
+  def skipper
+    User.find(skipper_id)
+  end
 end
